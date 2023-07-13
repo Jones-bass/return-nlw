deee52c971d33b7c59d13a48a748e49a89be929d
jones-bass <jonesbass.tb@gmail.com>
2023-07-13 17:10:56 -0300

    Component Button e WidgetForm

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,27 +1,23 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
-
+import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm';
 
 export function Widget() {
-    const [isWingetOpen, setIsWingetOpen] = useState(false);
-
-    function toggleWinWingetVisibility() {
-        setIsWingetOpen(!isWingetOpen)
-    }
-
 
     return (
-        <div className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end ">
-           { isWingetOpen ? <p>Hello World</p> : null }
+        <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end ">
+           <Popover.Panel>
+           <WidgetForm/>
+            </Popover.Panel>
 
-            <button onClick={toggleWinWingetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
                 <ChatTeardropDots className="w-6 h-6" />
 
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear select-none">
                     <span className="pl-2"></span>
                     Feedback
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+            </Popover>
     )
 }

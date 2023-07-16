632cc3bdaa838e44fdb3dd1e9d41c5b2eaea9f32
jones-bass <jonesbass.tb@gmail.com>
2023-07-16 19:56:34 -0300

    Component ScreenshotButton

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
 import { ArrowLeft } from 'phosphor-react';
+import { ScreenshotButton } from './ScreenshotButton';
+import { useState } from 'react';
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -12,6 +14,7 @@ export function FeedbackContentStep({
     onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
 
+    const [screenshot, setScreenshot] = useState<string | null>(null);
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
@@ -32,7 +35,25 @@ export function FeedbackContentStep({
 
                 <CloseButton />
             </header>
-            <div className='flex py-8 gap-2 w-full'></div>
+            <form className="my-4 w-full select-none">
+                <textarea
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                />
+
+                <footer className="flex gap-2 mt-2">
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
+
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+                    >Enviar Feedback
+                    </button>
+                </footer>
+            </form>
         </>
     )
 }
